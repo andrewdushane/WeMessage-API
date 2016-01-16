9cdea946c5f82be3e5065c17475bd93d45baf435
Andrew Dushane <andrewdushane@gmail.com>
2016-01-16 17:32:22 -0600

    basic messages routes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ActiveRecord::Base
+  validates :content, presence: true
+  validates :sender_account, presence: true
+  validates :recipient_account, presence: true
   belongs_to :sender, :class_name => 'Account', :foreign_key => 'sender_account'
   belongs_to :recipient, :class_name => 'Account', :foreign_key => 'recipient_account'
+  after_initialize :init
+
+  def init
+    self.delivered = false if self.delivered.nil?
+  end
 end

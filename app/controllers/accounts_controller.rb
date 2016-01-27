cb94dad0f9e2f436a6953631d145fa0d946ed5b4
Andrew Dushane <andrewdushane@gmail.com>
2016-01-27 09:28:49 -0600

    remove location designation from create route

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -46,7 +46,7 @@ class AccountsController < ApplicationController
     @account = Account.new(account_params)
 
     if @account.save
-      render json: @account, status: :created, location: @account
+      render json: @account, status: :created
     else
       render json: @account.errors, status: :unprocessable_entity
     end
